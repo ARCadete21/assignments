a284955176f5715328020405207fc67f7f6a5334
Afonso Roque Cadete <aroquecadete@gmail.com>
2024-11-11 21:15:34 +0000

    test: update pytest fixtures and unit tests

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,19 +1,33 @@
 """Tests for the cleaning module"""
+from unittest.mock import patch
 import pandas as pd
 
-from life_expectancy.cleaning import load_data, clean_data, save_data
+from life_expectancy.cleaning import clean_data
+from life_expectancy.loading_saving import load_data, save_data
 from . import OUTPUT_DIR
 
 
-def test_clean_data(pt_life_expectancy_expected):
-    """Run the `clean_data` function and compare the output to the expected output"""
-    raw_df = load_data()
-    cleaned_df = clean_data(raw_df, 'PT')
-    save_data(cleaned_df, 'PT')
+def test_load_data(eu_life_expectancy_raw):
+    """Test the load_data function"""
+    with patch('pandas.read_csv', return_value=eu_life_expectancy_raw):
+        df = load_data()
+        assert df.equals(eu_life_expectancy_raw)
 
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
-    pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
-    )
+
+def test_clean_data(eu_life_expectancy_raw, eu_life_expectancy_expected):
+    """Run the `clean_data` function and compare the output to the expected output"""
+    df = clean_data(eu_life_expectancy_raw, country='PT')
+    df.reset_index(drop=True, inplace=True)
+    pd.testing.assert_frame_equal(df, eu_life_expectancy_expected)
+
+
+def test_save_data():
+    """Test the save_data function"""
+    with patch('pandas.DataFrame.to_csv') as mock_to_csv:
+        df = pd.DataFrame({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6]
+        })
+        save_data(df, country='PT')        
+        expected_filepath = str(OUTPUT_DIR / 'pt_life_expectancy.csv')
+        mock_to_csv.assert_called_once_with(expected_filepath, index=False)
