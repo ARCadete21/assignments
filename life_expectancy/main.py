a9ef45d09f94134db6560e8135e5f781b7090bcf
Afonso Roque Cadete <aroquecadete@gmail.com>
2024-12-17 18:04:31 +0000

    feat: apply design strategies

--- life_expectancy/main.py
+++ life_expectancy/main.py
@@ -1,13 +1,19 @@
 import argparse
-from life_expectancy.cleaning import clean_data
-from life_expectancy.loading_saving import load_data, save_data
+import logging
+from typing import Tuple
 from life_expectancy.regions import Region
+from life_expectancy.handler import DataHandler, get_data_handler
+from life_expectancy.saving import save_data
 
 
-def main() -> None:
-    """Load, clean and save the DataFrame with country imputed."""
-    parser = argparse.ArgumentParser(
-        description="Clean data for a specified country."
+def get_arguments() -> Tuple[str, Region]:
+    """Get country code through Argument Parser."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--file_path', 
+        type=str,
+        default='eu_life_expectancy_raw.tsv',
+        help='The file relative path from the data to load (default is eu_life_expectancy_raw.tsv)'
     )
     parser.add_argument(
         '--country', 
@@ -16,11 +22,29 @@ def main() -> None:
         help='The country code for which to clean data (default is PT)'
     )
     args = parser.parse_args()
-
-    data = load_data()
-    data = clean_data(data, args.country)
-    save_data(data, args.country)
+    return args.file_path, args.country
 
 
-if __name__ == "__main__":
-    main()
+def main(data_handler: DataHandler, file_name: str, country_code: Region) -> None:
+    """Load, clean and save the DataFrame with country code imputed."""
+    logger = logging.getLogger(__name__)
+    logger.info('Loading data from %s...', file_name)
+    data = data_handler.load_data(file_name)
+    logger.info('Data loaded!')
+    logger.info('Cleaning data filtered by %s...', country_code.name)
+    data = data_handler.clean_data(data, country_code)
+    logger.info('Date cleaned!')
+    logger.info('Saving data...')
+    save_data(data, country_code)
+    logger.info('Data saved!')
+
+
+if __name__ == "__main__": # pragma: no cover
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s'
+    )
+    file, country = get_arguments()
+    file_title, file_extension = file.split('.')
+    handler = get_data_handler(file_extension)
+    main(handler, file_title, country)
--- life_expectancy/saving.py
+++ life_expectancy/saving.py
@@ -7,13 +7,6 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(SCRIPT_DIR, 'data')
 
 
-def load_data() -> pd.DataFrame:
-    """Load the TSV Life Expectancy DataFrame."""
-    input_data_path = os.path.join(DATA_PATH, 'eu_life_expectancy_raw.tsv')
-    df = pd.read_csv(input_data_path, sep='\t')
-    return df
-
-
 def save_data(df: pd.DataFrame, country: Region) -> None:
     """Save the DataFrame from a given country in the data folder."""
     output_file_name = f'{country.name.lower()}_life_expectancy.csv'
